3f425c62aeb0819effe37e2dbd1f08f6f8c0c34f
Txythread <txyzthread@icloud.com>
2025-12-02 21:52:50 +0100

    Officially Removed x86_64 support

--- targets/x86_64/reset-vector.s
+++
@@ -1,3 +0,0 @@
-# This file is for address 0xffff:0000 in unnormal real mode.
-# It does nothing but jump to 0x0, where the trampoline sits.
-jmpf 0x0
--- targets/x86_64/trampoline.s
+++
@@ -1,6 +0,0 @@
-	.section .text.trampoline
-	.align 2
-	.global _start_trampoline
-
-_start_trampoline:
-	jmp	_kernel_start
